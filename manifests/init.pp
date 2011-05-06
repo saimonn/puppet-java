97e330bec627764c5bf1aef8f7b12b67210ebc11
Mathieu Bornoz <mathieu.bornoz@camptocamp.com>
2011-05-06 14:28:43 +0200

    java: simplifies default module interface

--- manifests/init.pp
+++ manifests/init.pp
@@ -2,18 +2,7 @@ import "classes/*.pp"
 
 class java {
   case $operatingsystem {
-    Debian: {
-      case $lsbdistcodename {
-        etch,lenny,squeeze : { include java::v6 }
-        default: { fail "java::v6 not available for ${operatingsystem}/${lsbdistcodename}" }
-      }
-    }
-    Ubuntu: { 
-      case $lsbdistcodename {
-        hardy,intrepid,jaunty,lucid : { include java::v6 }
-        default: { fail "java::v6 not available for ${operatingsystem}/${lsbdistcodename}" }
-      }
-    }
+    Debian,Ubuntu,RedHat,CentOS: { include java::v6 }
     default: { notice "Unsupported operatingsystem ${operatingsystem}" }
   }
 
