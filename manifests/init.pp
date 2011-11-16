2a5f228afe9ebc16e6d1dd1e1a1d0eee5472fc1d
Marc Fournier <marc.fournier@camptocamp.com>
2011-11-16 11:13:08 +0100

    java: removed explicit import of inexisting manifests.

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,11 +1,6 @@
-import "classes/*.pp"
-import "definitions/*.pp"
-
 class java {
   case $operatingsystem {
     Debian,Ubuntu,RedHat,CentOS: { include java::v6 }
     default: { notice "Unsupported operatingsystem ${operatingsystem}" }
   }
-
 }
-
