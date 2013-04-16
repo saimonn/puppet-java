9e83bc8fd06cd6b2cc500450cf157d0f20ed34c7
Raphaël Pinson <raphael.pinson@camptocamp.com>
2013-04-16 13:40:22 +0200

    Use openssl::certificate::x509 and java_ks in jave::keystore::keypair

--- manifests/keystore/keypair.pp
+++ manifests/keystore/keypair.pp
@@ -8,8 +8,8 @@
 #   $name             - key alias in keystore
 #   $keystore         - keystore filename (mandatory)
 #   $basedir          - keystore location (mandatory)
-#   $keyalg           - key algorithm (default: RSA)
-#   $keysize          - key size (default: 2048)
+#   $keyalg           - key algorithm - DEPRECATED
+#   $keysize          - key size - DEPRECATED
 #   $storepass        - keystore password (default: changeit)
 #   $alias            - key pair alias (default: $name)
 #   $validity         - key pair validity (default: 3650 days)
@@ -28,18 +28,18 @@
 #
 #
 define java::keystore::keypair(
-  $ensure=present,
   $keystore,
   $basedir,
-  $keyalg='RSA',
-  $keysize=2048,
+  $country,
+  $organisation,
+  $ensure=present,
+  $keyalg=undef,
+  $keysize=undef,
   $storepass='changeit',
   $kalias='',
   $validity=3650,
   $commonName='localhost',
-  $organisationUnit='',
-  $organisation='',
-  $country='',
+  $organisationUnit=undef,
   $keypass='changeit',
 ) {
 
@@ -48,48 +48,28 @@ define java::keystore::keypair(
     default => $kalias,
   }
 
-  case $ensure {
-    present: {
-      $ou = $organisationUnit? {
-        ''      => '',
-        default => ",ou=${organisationUnit}",
-      }
-      $o = $organisation? {
-        ''      => '',
-        default => ",o=${organisation}",
-      }
-      $c = $country? {
-        ''      => '',
-        default => ",c=${country}",
-      }
+  if $keyalg {
+    fail '$keyalg is deprecated'
+  }
 
-      $dn = "cn=${commonName}${ou}${o}${c}"
-      exec {"java::key: Creates ${_kalias} to ${keystore}":
-        command => "keytool -genkeypair -keyalg ${keyalg} -keysize ${keysize} -keystore ${basedir}/${keystore} -storepass ${storepass} -storetype jks -alias ${_kalias} -validity ${validity} -dname '${dn}' -keypass ${keypass}",
-        unless  => "keytool -list -keystore ${basedir}/${keystore} -storepass ${storepass} -alias ${_kalias}"
-      }
+  if $keysize {
+    fail '$keysize is deprecated'
+  }
 
-      exec {"java::key: Export ${_kalias} keypair":
-        command => "keytool -exportcert -keystore ${basedir}/${keystore} -storepass ${storepass} -keypass ${keypass} -alias ${_kalias} -file ${basedir}/${_kalias}.crt",
-        creates => "${basedir}/${_kalias}.crt",
-        require => Exec["java::key: Creates ${_kalias} to ${keystore}"],
-      }
+  openssl::certificate::x509 { $_kalias:
+    ensure       => $ensure,
+    base_dir     => $basedir,
+    country      => $country,
+    organisation => $organisation,
+    unit         => $organisationUnit,
+    commonname   => $commonName,
+    days         => $validity,
+  }
 
-      exec {"java::key: Generate CSR for ${_kalias}":
-        command => "keytool -certreq -keystore ${basedir}/${keystore} -storepass ${storepass} -alias ${_kalias} -keypass ${keypass} -file ${basedir}/${_kalias}.csr",
-        creates => "${basedir}/${_kalias}.csr",
-        require => Exec["java::key: Creates ${_kalias} to ${keystore}"],
-      }
-
-    }
-    absent: {
-      exec {"java::key: Remove ${_kalias} from ${keystore}":
-        command => "keytool -delete -alias ${_kalias} -keystore ${basedir}/${keystore} -storepass ${storepass}",
-        onlyif  => "keytool -list -keystore ${keystore} -storepass ${storepass} -alias ${_kalias}",
-      }
-      file {["${basedir}/${_kalias}.csr", "${basedir}/${_kalias}.crt"]:
-        ensure => absent,
-      }
-    }
+  java_ks { "${_kalias}:${basedir}/${keystore}":
+    ensure      => $ensure,
+    certificate => "${basedir}/${_kalias}.crt",
+    private_key => "${basedir}/${_kalias}.key",
+    password    => $storepass,
   }
 }
